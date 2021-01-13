b207e6ed8a0451d69a75b9cda7f4055688c57139
Sequal32 <connor0530@gmail.com>
2021-01-13 17:39:33 -0500

    rolling average

--- src/infrastructure/metrics.rs
+++ src/infrastructure/metrics.rs
@@ -1,10 +1,13 @@
+use std::cmp::min;
+
+const FACTOR: u32 = 15;
 /// Metrics to be sent every second 
 #[derive(Clone, Copy, PartialEq, Debug)]
 pub struct Metrics {
     /// Packets sent
-    pub sent_packets: usize,
+    pub sent_packets: f32,
     /// Packets received
-    pub received_packets: usize,
+    pub received_packets: f32,
     /// The amount of data in kilobytes sent
     pub sent_kbps: f32,
     /// The amount of data in kilobytes received
@@ -18,8 +21,8 @@ pub struct Metrics {
 impl Default for Metrics {
     fn default() -> Self {
         Self {
-            sent_packets: 0,
-            received_packets: 0,
+            sent_packets: 0.0,
+            received_packets: 0.0,
             sent_kbps: 0.0,
             receive_kbps: 0.0,
             packet_loss: 0.0,
@@ -28,77 +31,57 @@ impl Default for Metrics {
     }
 }
 
-/// A "frame" of metrics, where all the values in Vecs will be averaged into a Metrics struct
-struct MetricsArray {
-    pub sent_kbps: Vec<f32>,
-    pub receive_kbps: Vec<f32>,
-    pub packet_loss: usize,
-    pub rtt: Vec<f32>
-}
-
-impl Default for MetricsArray {
-    fn default() -> Self {
-        Self {
-            sent_kbps: Vec::new(),
-            receive_kbps: Vec::new(),
-            packet_loss: 0,
-            rtt: Vec::new(),
-        }
-    }
-}
-
-impl MetricsArray {
-    pub fn reset(&mut self) {
-        self.sent_kbps.clear();
-        self.receive_kbps.clear();
-        self.rtt.clear();
-        self.packet_loss = 0;
-    }
-}
-
 pub struct MetricsHandler {
-    current_frame: MetricsArray,
+    current_frame: Metrics,
+    current_averages: Metrics,
+    counter: u32
 }
 
 impl MetricsHandler {
     pub fn new() -> Self {
         Self {
-            current_frame: MetricsArray::default(),
+            current_frame: Metrics::default(),
+            current_averages: Metrics::default(),
+            counter: 0
         }
     }
 
     pub fn record_sent_info(&mut self, sent_bytes: usize) {
-        self.current_frame.sent_kbps.push(sent_bytes as f32/1000.0);
+        self.current_frame.sent_kbps += sent_bytes as f32/1000.0;
+        self.current_frame.sent_packets += 1.0;
     }
 
     pub fn record_receive_info(&mut self, receive_bytes: usize) {
-        self.current_frame.receive_kbps.push(receive_bytes as f32/1000.0);
+        self.current_frame.receive_kbps += receive_bytes as f32/1000.0;
+        self.current_frame.received_packets += 1.0;
     }
 
     pub fn record_packet_loss(&mut self, dropped_packets_count: usize) {
-        self.current_frame.packet_loss += dropped_packets_count;
+        self.current_frame.packet_loss += dropped_packets_count as f32;
     }
 
     pub fn record_rtt(&mut self, rtt: f32) {
-        self.current_frame.rtt.push(rtt);
+        self.current_frame.rtt = rtt;
+    }
+
+    fn average(&self, new_value: f32, average: f32) -> f32 {
+        return average + (new_value - average) / min(self.counter, FACTOR) as f32;
     }
 
     // Should be called every second
     pub fn calculate_output(&mut self) -> Metrics {
-        let sent_count = self.current_frame.sent_kbps.len();
-        let receive_count = self.current_frame.receive_kbps.len();
-
-        let result = Metrics {
-            sent_packets: sent_count,
-            received_packets: receive_count,
-            sent_kbps: if sent_count == 0 {0.0} else {self.current_frame.sent_kbps.iter().fold(0.0, |acc, x| acc + x)/sent_count as f32},
-            receive_kbps: if receive_count == 0 {0.0} else {self.current_frame.receive_kbps.iter().fold(0.0, |acc, x| acc + x)/receive_count as f32},
-            packet_loss: if sent_count == 0 {0.0} else {self.current_frame.packet_loss as f32/sent_count as f32},
-            rtt: 0.0,
+        self.counter += 1;
+        self.current_averages = Metrics {
+            sent_packets: self.average(self.current_frame.sent_packets, self.current_averages.sent_packets),
+            received_packets: self.average(self.current_frame.received_packets, self.current_averages.received_packets),
+            sent_kbps: self.average(self.current_frame.sent_kbps, self.current_averages.sent_kbps),
+            receive_kbps: self.average(self.current_frame.receive_kbps, self.current_averages.receive_kbps),
+            packet_loss: self.average(if self.current_frame.sent_packets > 0.0 {self.current_frame.packet_loss/self.current_frame.sent_packets} else {0.0}, self.current_averages.packet_loss),
+            rtt: self.average(self.current_frame.rtt, self.current_averages.rtt),
         };
 
-        self.current_frame.reset();
-        
-        result
+        self.current_frame = Metrics::default();
+
+        self.current_averages.clone()
     }
 }
